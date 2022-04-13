85bdbab5c9eeaae5d861b6f1021939b7e59ee8a8
AnaPaulaKabum <ana.desouza@kabum.com.br>
2022-04-13 16:29:24 -0300

    remove async and awiat

--- test/itemsService.test.ts
+++ test/itemsService.test.ts
@@ -5,41 +5,41 @@ var sinon = require('sinon');
 
 const itemsService = new ItemService(new ItemRepository());
 
-test('Deve obter os itens', async function () {
-  const items = await itemsService.getItems();
+test('Deve obter os itens', function () {
+  const items = itemsService.getItems();
   expect(items).toHaveLength(3);
 });
 
-test('Deve obter os itens com stub', async function () {
+test('Deve obter os itens com stub', function () {
   sinon.stub(itemsService, 'getItems').returns([{ id_item: 4 }]);
-  const items = await itemsService.getItems();
+  const items = itemsService.getItems();
   expect(items).toHaveLength(1);
   sinon.restore();
 });
 
-test('Deve obter os itens com spy', async function () {
+test('Deve obter os itens com spy', function () {
   const spy = sinon.spy(itemsService, 'getItems');
-  const items = await itemsService.getItems();
+  const items = itemsService.getItems();
   expect(items).toHaveLength(3);
   sinon.assert.calledOnce(spy);
   sinon.restore();
 });
 
-test('Deve obter os itens com mock', async function () {
+test('Deve obter os itens com mock', function () {
   const mock = sinon.mock(itemsService);
   mock
     .expects('getItems')
     .once()
     .returns([{ id_item: 4 }]);
-  const items = await itemsService.getItems();
+  const items = itemsService.getItems();
   expect(items).toHaveLength(1);
   mock.verify();
   sinon.restore();
 });
 
-test('Deve obter os itens com fake', async function () {
+test('Deve obter os itens com fake', function () {
   const itemsServiceFake = new ItemService(new ItemRepositoryFake());
 
-  const items = await itemsServiceFake.getItems();
+  const items = itemsServiceFake.getItems();
   expect(items).toHaveLength(1);
 });
